6d875bf9d7560b37270f0bdebc89e85f484f021a
SeungJun Baek <21900346@handong.edu>
2020-04-29 17:40:23 +0900

    Modifying Test.java

--- src/main/java/edu/handong/csee/java/Test.java
+++ src/main/java/edu/handong/csee/java/Test.java
@@ -3,7 +3,7 @@ package edu.handong.csee.java;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Test this");
+		System.out.println("This is for test.");
 
 	}
 
